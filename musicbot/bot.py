79bc69c1ea09ecffbefbf0410dbe234c9d3aae01
Johan Ribom <jribom@kth.se>
2017-04-03 00:51:36 +0200

    :construction: Improve the list length check in cmd_playprio

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -846,7 +846,7 @@ class MusicBot(discord.Client):
 
     async def cmd_playprio(self, player, channel, author, permissions, leftover_args, song_url):
         response = await self.cmd_play(player, channel, author, permissions, leftover_args, song_url)
-        if re.match(r"Position in queue: \d", response.content):
+        if len(self.player.playlist.entries) > 1:
             return await self.cmd_prio(str(len(self.player.playlist.entries)))
         return response
 
